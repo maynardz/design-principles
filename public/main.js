6b3c86988201b8b17931f1f84f00cc21e156f8a2
Zach Maynard <maynard.zach23@gmail.com>
2019-05-01 03:16:20 -0400

    media queries

---
+++ public/main.js
@@ -0,0 +1,34 @@
+let bars = document.getElementById("nav-action");
+let nav = document.getElementById("nav");
+
+
+bars.addEventListener("click", barClicked, false);
+
+function barClicked() {
+  bars.classList.toggle('active');
+  nav.classList.toggle('visible');
+}
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+actualWidth = $tip[0].offsetWidth
+        actualHeight = $tip[0].offsetHeight
+
+        switch (placement) {
+          case 'bottom':
+            tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2}
+            break
+          case 'top':
+            tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2}
+            break
+          case 'left':
+            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth}
+            break
+          case 'right':
+            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width}
+            break
+        }
+
+        this.applyPlacement(tp, placement)
